34eceb162d4ea5b7a1662a4ca0a5bbe01ec591bd
eobaah <eobaah@gmail.com>
2017-03-20 08:46:10 -0700

    Vanilla Javascript project for review

---
+++ arraysBasics.js
@@ -0,0 +1,14 @@
+// Is array copied? What is the following code going to show?
+let fruits = ["Apples", "Pear", "Orange"];
+
+// push a new value into the "copy"
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+// what's in fruits?
+alert( fruits.length ); // 4
+
+// Array operations
+
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
